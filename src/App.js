b3dfe4b9411543dddf1c008f6838231c7331e0d7
kamil26300 <kamil.dehliwala2021@vitstudent.ac.in>
2023-04-02 15:02:45 +0530

    good

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 	const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=6f8d314c84e820cd935e8e5c7ecb7125&language-en-US&query=${searchText}&page=1&include_adult=false`)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=6f8d314c84e820cd935e8e5c7ecb7125&language-en-US&query=${searchText}&page=1&include_adult=true`)
         .then(response => response.json())
         .then(data => {
             setSearchResults(data.results)
--- src/components/Searchview.js
+++ src/components/Searchview.js
@@ -10,18 +10,22 @@ const ReadMore = ({ children }) => {
 		setIsReadMore(!isReadMore);
 	};
 
-    if (text.length > 155) {
-        return (
-            <p className="text">
-                {isReadMore ? text.slice(0, 150) : text}
-                <span onClick={toggleReadMore} className="read-or-hide">
-                    {isReadMore ? "...read more" : " show less"}
-                </span>
-            </p>
-        );
-    } else {
-        return <p>{text}</p>;
-    }
+	if (text.length > 155) {
+		return (
+			<p className="text">
+				{isReadMore ? text.slice(0, 150) : text}
+				<span
+					onClick={toggleReadMore}
+					className="read-or-hide">
+					{isReadMore
+						? "...read more"
+						: " show less"}
+				</span>
+			</p>
+		);
+	} else {
+		return <p>{text}</p>;
+	}
 };
 
 const Searchview = ({ keyword, results }) => {
@@ -31,36 +35,41 @@ const Searchview = ({ keyword, results }) => {
 		if (obj.backdrop_path) {
 			const posterURL = `https://image.tmdb.org/t/p/original${obj.backdrop_path}`;
 			return (
-				<div key={i} className="card bg-dark text-light">
-					<img
-						src={posterURL}
-						className="card-img-top"
-						alt="poster.jpg"
-					/>
-					<div className="card-body">
-						<h5 className="card-title">
-							{obj.original_title}
-						</h5>
-                        <ReadMore>
-							{obj.overview}
-                        </ReadMore>
-						<a
-							href="#"
-							className="btn cardB btn-primary">
-							Open
-						</a>
+				<div key={i} className="col-lg-4 col-md-6 col-12 py-2">
+					<div
+						className="card bg-dark text-light">
+						<img
+							src={posterURL}
+							className="card-img-top"
+							alt="poster.jpg"
+						/>
+						<div className="card-body">
+							<h5 className="card-title">
+								{
+									obj.original_title
+								}
+							</h5>
+							<ReadMore>
+								{obj.overview}
+							</ReadMore>
+							<a
+								href="#"
+								className="btn cardB btn-primary">
+								Open
+							</a>
+						</div>
 					</div>
 				</div>
 			);
 		} else {
-            return <></>
-        }
+			return <></>;
+		}
 	});
 	return (
 		<div>
 			<Hero text={title} />
-			<div className="d-flex cardDiv flex-wrap m-0">
-				{resultHtml}
+			<div className="container cardDiv">
+				<div className="row py-3">{resultHtml}</div>
 			</div>
 		</div>
 	);
